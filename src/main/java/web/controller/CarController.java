192a211731c4ca33773e07d78b2c949890d1f4a7
Alexei Mechetin <103155422+AlexMenov@users.noreply.github.com>
2023-08-21 12:50:52 +0700

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,46 +1,26 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.services.CarService;
-import java.util.List;
 
 @Controller
 public class CarController {
-    private final ApplicationContext applicationContext;
-    private List<Car> cars;
-
+    private final CarService carService;
     @Autowired
-    public CarController(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-        CarService carService = getApplicationContext().getBean(CarService.class);
-        setCars(carService.getCars());
-    }
-
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
-    }
-
-    public ApplicationContext getApplicationContext() {
-        return applicationContext;
+    public CarController (CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        if (count > getCars().size()) {
-            count = getCars().size();
+        if (count > 5) {
+            count = 5;
         }
-        model.addAttribute("cars", getCars().subList(0, count));
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
-
